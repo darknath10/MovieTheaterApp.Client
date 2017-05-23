745d9f17181a53135e76e2276b67546c6e2a63e9
Nathan Preel <nathan.preel@gmail.com>
2017-05-23 12:31:44 +0300

    Working on movie list layout

--- src/app/movies/movie-list/movie-list.component.ts
+++ src/app/movies/movie-list/movie-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { MovieService } from '../services/movie.service';
+import { ScreenService } from '../../services/screen.service';
 
 import { IMovie } from '../movie.model';
 
@@ -11,11 +12,13 @@ import { IMovie } from '../movie.model';
 export class MovieListComponent implements OnInit {
   movies: IMovie[];
 
-  constructor(private movieService: MovieService) { }
+  constructor(private movieService: MovieService,
+    private screen: ScreenService) { }
 
   ngOnInit() {
     this.movieService.getMovies().subscribe(movies => this.movies = movies.map((movie: IMovie) => {
       movie.poster_path = `http://image.tmdb.org/t/p/original/${movie.poster_path}`;
+      movie.backdrop_path = `http://image.tmdb.org/t/p/original/${movie.backdrop_path}`;
       return movie;
     }));
   }
