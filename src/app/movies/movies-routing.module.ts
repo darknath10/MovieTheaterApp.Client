4d64a19c30f60f35a52545eb46c0e216d720b12a
Nathan Preel <nathan.preel@gmail.com>
2017-05-29 01:11:37 +0300

    add resolve property in movies route

--- src/app/movies/movies-routing.module.ts
+++ src/app/movies/movies-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { MovieListResolver } from './services/movie-list-resolver.service';
+
 import { MovieListComponent } from './movie-list/movie-list.component';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 
 const moviesRoutes: Routes = [
-    { path: 'movies', component: MovieListComponent },
+    { path: 'movies', component: MovieListComponent, resolve: {movies: MovieListResolver} },
     { path: 'movies/:id', component: MovieDetailComponent }
 ];
 
