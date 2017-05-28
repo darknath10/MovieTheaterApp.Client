d98fc867eadf15d0a3d75ee1e66ddf435479c0ed
Nathan Preel <nathan.preel@gmail.com>
2017-05-29 01:10:10 +0300

    add MovieListResolver service

---
+++ src/app/movies/services/movie-list-resolver.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Resolve } from '@angular/router';
+
+import { MovieService } from './movie.service';
+
+import { IMovie } from '../movie.model';
+
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class MovieListResolver implements Resolve<IMovie[]> {
+
+  constructor(private movieService: MovieService) { }
+
+  resolve(): Observable<IMovie[]> {
+    return this.movieService.getMovies();
+  }
+}
