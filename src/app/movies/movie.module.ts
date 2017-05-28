93b405411d44b4110fc00b86838c13191b377ec7
Nathan Preel <nathan.preel@gmail.com>
2017-05-29 01:10:57 +0300

    register MovieListResolver in movie module providers

--- src/app/movies/movie.module.ts
+++ src/app/movies/movie.module.ts
@@ -7,6 +7,7 @@ import { MovieRoutingModule } from './movies-routing.module';
 import { MovieListComponent } from './movie-list/movie-list.component';
 
 import { MovieService } from './services/movie.service';
+import { MovieListResolver } from './services/movie-list-resolver.service';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 
@@ -16,6 +17,9 @@ import { MovieDetailComponent } from './movie-detail/movie-detail.component';
     MovieRoutingModule  
   ],
   declarations: [MovieListComponent, MovieCardComponent, MovieDetailComponent],
-  providers: [MovieService]
+  providers: [
+    MovieService,
+    MovieListResolver
+  ]
 })
 export class MovieModule {}
