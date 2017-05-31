7fdb4de9c754052617c38bfdd38fb97066d72891
Nathan Preel <nathan.preel@gmail.com>
2017-05-31 08:16:47 +0300

    Removes ScreenService DI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
 
-import { ScreenService } from './services/screen.service';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  
-  constructor(private screen: ScreenService) {}
-}
+export class AppComponent { }
