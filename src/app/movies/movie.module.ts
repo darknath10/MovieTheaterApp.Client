3293684991ef11ae053f25f4c1dd24216549902c
Nathan Preel <nathan.preel@gmail.com>
2017-06-01 15:13:13 +0300

    update movie.module and movies-routing-module

--- src/app/movies/movie.module.ts
+++ src/app/movies/movie.module.ts
@@ -1,24 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { MovieRoutingModule } from './movies-routing.module';
 
 import { MovieListComponent } from './movie-list/movie-list.component';
 
 import { MovieService } from './services/movie.service';
+import { TmdbService } from './services/tmdb.service';
 import { MovieListResolver } from './services/movie-list-resolver.service';
 import { MovieDetailResolver } from './services/movie-detail-resolver.service';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { AddMovieComponent } from './add-movie/add-movie.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     MovieRoutingModule  
   ],
-  declarations: [MovieListComponent, MovieCardComponent, MovieDetailComponent],
+  declarations: [MovieListComponent, MovieCardComponent, MovieDetailComponent, AddMovieComponent],
   providers: [
+    TmdbService,
     MovieService,
     MovieListResolver,
     MovieDetailResolver
--- src/app/movies/movies-routing.module.ts
+++ src/app/movies/movies-routing.module.ts
@@ -6,9 +6,11 @@ import { MovieDetailResolver } from './services/movie-detail-resolver.service';
 
 import { MovieListComponent } from './movie-list/movie-list.component';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { AddMovieComponent } from './add-movie/add-movie.component';
 
 const moviesRoutes: Routes = [
     { path: 'movies', component: MovieListComponent, resolve: {movies: MovieListResolver} },
+    { path: 'movies/new', component: AddMovieComponent },
     { path: 'movies/:id', component: MovieDetailComponent, resolve: {movie: MovieDetailResolver} }
 ];
 
