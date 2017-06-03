5c1803042da424557035090891a0b871b664ef6f
Nathan Preel <nathan.preel@gmail.com>
2017-06-03 16:49:41 +0300

    add toastr.service and declare it in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 import { ScreenService } from './services/screen.service';
+import { TOASTR_TOKEN, IToastr } from './services/toastr.service';
+
+declare let toastr: IToastr;
 
 @NgModule({
   declarations: [
@@ -22,7 +25,10 @@ import { ScreenService } from './services/screen.service';
     MovieModule,
     AppRoutingModule
   ],
-  providers: [ScreenService],
+  providers: [
+    ScreenService,
+    { provide: TOASTR_TOKEN, useValue: toastr }
+    ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
