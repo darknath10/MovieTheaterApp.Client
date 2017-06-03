fef65484cba64d39e713a53ef5aa9ecfbd06d833
Nathan Preel <nathan.preel@gmail.com>
2017-06-03 16:56:07 +0300

    Add getMovieDetails function to service

--- src/app/movies/services/tmdb.service.ts
+++ src/app/movies/services/tmdb.service.ts
@@ -6,6 +6,9 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 
 import { IMovie } from '../movie.model';
+import { ITmdbMovie } from '../tmdb-movie.model';
+
+import { tmdbMovieMapper } from '../helpers/tmdbMovieMapper';
 
 @Injectable()
 export class TmdbService {
@@ -18,11 +21,17 @@ export class TmdbService {
       .catch(this.handleError);
   }
 
+  getMovieDetails(id: number): Observable<ITmdbMovie> {
+    return this.http.get(`https://api.themoviedb.org/3/movie/${id}?api_key=d1dd5a7fd77c933e088112709eb711e7&language=en-US&append_to_response=videos`)
+      .map((response: Response) => <ITmdbMovie>response.json())
+      .catch(this.handleError);
+  }
+
   private handleError(error: Response) {
-        // in a real world app, we may send the server to some remote logging infrastructure
-        // instead of just logging it to the console
-        console.error(error);
-        return Observable.throw(error.json() || 'Server error');
-    }
+    // in a real world app, we may send the server to some remote logging infrastructure
+    // instead of just logging it to the console
+    console.error(error);
+    return Observable.throw(error.json() || 'Server error');
+  }
 
 }
