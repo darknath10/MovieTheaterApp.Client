9e89b70ffa9a07029e619b825bbd5a518f1fbe0e
darknath <nathan.preel@gmail.com>
2017-06-04 19:03:15 +0300

    Add movie-shows.component

---
+++ src/app/movies/movie-shows/movie-shows.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'movie-shows',
+  templateUrl: './movie-shows.component.html',
+  styleUrls: ['./movie-shows.component.css']
+})
+export class MovieShowsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
