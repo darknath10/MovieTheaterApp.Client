dc30fd0bd5c258ab73b30ce93c86a06af9a40163
darknath <nathan.preel@gmail.com>
2017-06-04 19:06:16 +0300

    Add child routes

--- src/app/movies/movies-routing.module.ts
+++ src/app/movies/movies-routing.module.ts
@@ -7,19 +7,52 @@ import { MovieDetailResolver } from './services/movie-detail-resolver.service';
 import { MovieListComponent } from './movie-list/movie-list.component';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 import { AddMovieComponent } from './add-movie/add-movie.component';
+import { MovieTrailerComponent } from './movie-trailer/movie-trailer.component';
+import { MovieShowsComponent } from './movie-shows/movie-shows.component';
+import { EditMovieComponent } from './edit-movie/edit-movie.component';
 
 const moviesRoutes: Routes = [
-    { path: 'movies', component: MovieListComponent, resolve: {movies: MovieListResolver} },
-    { path: 'movies/new', component: AddMovieComponent },
-    { path: 'movies/:id', component: MovieDetailComponent, resolve: {movie: MovieDetailResolver} }
+    {
+        path: 'movies',
+        children: [
+            {
+                path: '',
+                component: MovieListComponent,
+                resolve: {movies: MovieListResolver}
+            },
+            {
+                path: 'new',
+                component: AddMovieComponent
+            },
+            {
+                path: ':id',
+                component: MovieDetailComponent,
+                resolve: { movie: MovieDetailResolver },
+                children: [
+                    {
+                        path: '',
+                        component: MovieTrailerComponent
+                    },
+                    {
+                        path: 'shows',
+                        component: MovieShowsComponent
+                    },
+                    {
+                        path: 'edit',
+                        component: EditMovieComponent
+                    }
+                ]
+            }
+        ]
+    }
 ];
 
 @NgModule({
-  imports: [
-      RouterModule.forChild(moviesRoutes)
-  ],
-  exports: [
-      RouterModule
-  ]
+    imports: [
+        RouterModule.forChild(moviesRoutes)
+    ],
+    exports: [
+        RouterModule
+    ]
 })
 export class MovieRoutingModule { }
