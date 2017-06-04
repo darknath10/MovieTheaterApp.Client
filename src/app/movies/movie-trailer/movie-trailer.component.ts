d7265cbc9664858216790c68d8c9dbe0a5702405
darknath <nathan.preel@gmail.com>
2017-06-04 19:02:54 +0300

    Add movie-trailer.component

---
+++ src/app/movies/movie-trailer/movie-trailer.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
+import { IMovie } from '../movie.model';
+
+@Component({
+  templateUrl: './movie-trailer.component.html',
+  styleUrls: ['./movie-trailer.component.css']
+})
+export class MovieTrailerComponent implements OnInit {
+
+  movie: IMovie;
+  movieUrl: SafeResourceUrl;
+
+  constructor(private route: ActivatedRoute, private sanitizer: DomSanitizer) { }
+
+  ngOnInit() {
+    this.route.parent.data.subscribe(data => {
+      this.movie = data['movie'];
+      this.movieUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.movie.trailer_path);
+    });
+  }
+
+}
