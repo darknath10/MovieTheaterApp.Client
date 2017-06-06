79de603a7fd8d6c5f2060bf93311062e6d7a1410
Nathan Preel <nathan.preel@gmail.com>
2017-06-07 00:17:41 +0300

    implement the edot movie form

--- src/app/movies/edit-movie/edit-movie.component.ts
+++ src/app/movies/edit-movie/edit-movie.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { IMovie } from '../movie.model';
 
 @Component({
   selector: 'edit-movie',
@@ -6,10 +10,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-movie.component.css']
 })
 export class EditMovieComponent implements OnInit {
+  movie: IMovie;
+  editMovieForm: FormGroup;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.route.parent.data.subscribe(data => this.movie = data['movie']);
+    this.editMovieForm = this.fb.group({
+      backdrop_path: '',
+      tmdb_id: '',
+      overview: '',
+      popularity: '',
+      poster_path: '',
+      release_date: '',
+      runtime: '',
+      status: '',
+      tagline: '',
+      title: ['', Validators.required],
+      trailer_path: '',
+      vote_average: '',
+      vote_count: '',
+      genres: ''
+    });
+
+    this.editMovieForm.setValue({
+      backdrop_path: this.movie.backdrop_path,
+      tmdb_id: this.movie.tmdb_id,
+      overview: this.movie.overview,
+      popularity: this.movie.popularity,
+      poster_path: this.movie.poster_path,
+      release_date: this.movie.release_date,
+      runtime: this.movie.runtime,
+      status: this.movie.status,
+      tagline: this.movie.tagline,
+      title: this.movie.title,
+      trailer_path: this.movie.trailer_path,
+      vote_average: this.movie.vote_average,
+      vote_count: this.movie.vote_count,
+      genres: this.movie.genres
+    });
   }
 
+  save(): void {
+    let movieToSave: IMovie = Object.assign({}, this.movie, this.editMovieForm.value);
+    console.log(movieToSave);
+  }
 }
