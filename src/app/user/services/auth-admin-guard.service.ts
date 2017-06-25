efe1b4b1b75d7d11e2040dfb33e2d7078b6c2af4
Nathan Preel <nathan.preel@gmail.com>
2017-06-25 16:01:28 +0300

    upgrade service

--- src/app/user/services/auth-admin-guard.service.ts
+++ src/app/user/services/auth-admin-guard.service.ts
@@ -1,14 +1,17 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
 import { AuthService } from './auth.service';
+import { IToastr, TOASTR_TOKEN } from '../../services/toastr.service';
 
 import { IUser } from '../models/user.model';
 
 @Injectable()
 export class AuthAdminGuard implements CanActivate {
 
-  constructor(private auth: AuthService, private router: Router) { }
+  constructor(private auth: AuthService, 
+    private router: Router,
+    @Inject(TOASTR_TOKEN) private toastr: IToastr) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     return this.checkAdminLoggedIn(state.url);
@@ -16,6 +19,10 @@ export class AuthAdminGuard implements CanActivate {
 
   checkAdminLoggedIn(url: string): boolean {
     if(this.auth.isSuperUser()) return true;
+    if(!!this.auth.currentUser) {
+      this.toastr.info("You don't have permission to go there.");
+      return false;
+    }
     this.auth.redirectUrl = url;
     this.router.navigate(['/user/login']);
     return false;
