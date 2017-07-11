1f8387bb03546f966e812d648f8b28f258483bbd
darknath <nathan.preel@gmail.com>
2017-07-11 23:40:11 +0300

    refractor the code a little bit

--- src/app/movies/services/tmdb.service.ts
+++ src/app/movies/services/tmdb.service.ts
@@ -13,16 +13,19 @@ import { tmdbMovieMapper } from '../helpers/tmdbMovieMapper';
 @Injectable()
 export class TmdbService {
 
+  private _tmdbApiBaseUrl = 'https://api.themoviedb.org/3';
+  private _apiKey = 'd1dd5a7fd77c933e088112709eb711e7';
+
   constructor(private http: Http) { }
 
   searchMovie(term: string): Observable<IMovie[]> {
-    return this.http.get(`https://api.themoviedb.org/3/search/movie?api_key=d1dd5a7fd77c933e088112709eb711e7&query=${term}`)
+    return this.http.get(`${this._tmdbApiBaseUrl}/search/movie?api_key=${this._apiKey}&query=${term}`)
       .map((response: Response) => <IMovie[]>response.json())
       .catch(this.handleError);
   }
 
   getMovieDetails(id: number): Observable<ITmdbMovie> {
-    return this.http.get(`https://api.themoviedb.org/3/movie/${id}?api_key=d1dd5a7fd77c933e088112709eb711e7&language=en-US&append_to_response=videos`)
+    return this.http.get(`${this._tmdbApiBaseUrl}/movie/${id}?api_key=${this._apiKey}&language=en-US&append_to_response=videos`)
       .map((response: Response) => <ITmdbMovie>response.json())
       .catch(this.handleError);
   }
