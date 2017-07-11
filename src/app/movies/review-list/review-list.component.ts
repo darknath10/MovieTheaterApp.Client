9843d532dc21b20b736cb691314319d61e780d88
darknath <nathan.preel@gmail.com>
2017-07-11 22:58:44 +0300

    implement component functionality

--- src/app/movies/review-list/review-list.component.ts
+++ src/app/movies/review-list/review-list.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { TOASTR_TOKEN, IToastr } from '../../services/index';
+import { AuthService } from '../../user/services/auth.service';
+import { MovieService } from '../services/movie.service';
+
+import { IMovie } from '../models/movie.model';
 import { IReview } from '../models/review.model';
 
 @Component({
@@ -9,12 +14,35 @@ import { IReview } from '../models/review.model';
   styleUrls: ['./review-list.component.css']
 })
 export class ReviewListComponent implements OnInit {
+  movie: IMovie;
   reviews: IReview[];
+  addReviewMode: boolean = false;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, 
+    private auth: AuthService, 
+    private movieService: MovieService,
+    @Inject(TOASTR_TOKEN) private toastr: IToastr) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => this.reviews = data['reviews']);
+    this.route.parent.data.subscribe(data => this.movie = data['movie']);
+  }
+
+  userHasReview(): boolean {
+    if(!!this.auth.currentUser) return this.reviews.some(r => r.user['userName'] === this.auth.currentUser.username);
+    return true;
+  }
+
+  toggleAddReviewMode(): void {
+    this.addReviewMode = !this.addReviewMode;
+  }
+
+  addReview(review) {
+    this.movieService.addMovieReview(review, this.movie.id).subscribe(data => {
+      this.toastr.success('Thanks for reviewing');
+      this.reviews.push(data);
+      this.toggleAddReviewMode();
+    }, (error: Error) => this.toastr.error('An error occured. Try again.'));
   }
 
 }
